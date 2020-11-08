f3ccbfc6e5ab5d8e12e648d1c3fc085e2b407d50
David <davidirvin47@gmail.com>
2020-11-07 18:50:04 -0700

    reploy 7

--- app.js
+++ app.js
@@ -31,12 +31,7 @@ const listSchema = {
 
 const List = mongoose.model("List", listSchema);
 
-if(process.env.NODE_ENV === "production") {
-    //set static folder
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")));
-  }
+
 
 app.get("/:listName", (req, res) => {
     const newDay = req.params.listName;
@@ -85,3 +80,9 @@ app.post("/delete", (req, res) => {
     })
 })
 
+if(process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")));
+  }
