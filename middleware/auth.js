606836a46f5c1dbaf70ea7d2af4dc5b4a376e028
David <davidirvin47@gmail.com>
2021-02-01 15:53:50 -0700

    fixes auth error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const config = require("config");
+// const config = require("config");
 
 module.exports = function (req, res, next) {
   // get token from header
@@ -11,7 +11,7 @@ module.exports = function (req, res, next) {
   }
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decoded = jwt.verify(token, process.env.SECRET);
 
     req.user = decoded.user;
     next();
