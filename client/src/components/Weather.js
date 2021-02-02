5aa3c061c86bbcdfe1b23599e90b19a2323eb95b
David <davidirvin47@gmail.com>
2021-02-02 12:28:28 -0700

    fixes weather api

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,27 +1,45 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CheckIcon from '@material-ui/icons/Check';
 
 
 const Weather = () => {
   // let apiKey = process.env.REACT_APP_WEATHER;
+    
+
+  useEffect(() => {
+    var city = localStorage.getItem("city")
+    city && setLocation(city)
+    initialWeather();
+  }, [])
 
   const [weather, setWeather] = useState(null);
   const [icon, setIcon] = useState("");
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState("");
 
-  const getLocation = async () => {
-    const res = await axios.get("https://extreme-ip-lookup.com/json/");
-    setLocation(res.data.city);
-    // location && getWeather();
-  };
+  // const getLocation = async () => {
+  //   const res = await axios.get("https://extreme-ip-lookup.com/json/");
+  //   setLocation(res.data.city);
+  // };
 
-  useEffect(() => {
+  const initialWeather = () => {
     getWeather()
-  }, [location])
+  }
+
+  const newLocation = (e) => {
+    setLocation(e.target.value)
+  }
+
+  const submitCity = (e) => {
+    e.preventDefault();
+    localStorage.setItem("city", location)
+    getWeather();
+  }
 
 
   const getWeather = async () => {
-    if (location !== null) {
+    if (location !== "") {
+      setLocation(location.toLowerCase().replace(/ /g, "+"))
       try {
         const res = await axios.post("/weather", {location});
         console.log(res);
@@ -35,18 +53,18 @@ const Weather = () => {
     
   };
 
- useEffect(() => {
-    getLocation();
-    setInterval(() => {getLocation()}, 60000);
-  //eslint-disable-next-line
-  }, []);
+//  useEffect(() => {
+//     getLocation();
+//     setInterval(() => {getLocation()}, 60000);
+//   //eslint-disable-next-line
+//   }, []);
 
   
   // const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
   if (weather !== null) {
     return (
-      <div className="weather">
+      <div className="weather" onClick={() => setWeather(null)}>
         
         <h6 className="temp">
           {weather}°
@@ -58,7 +76,18 @@ const Weather = () => {
     );
   }
   return (
-    <div></div>
+    <form className="weather-input__wrapper" onSubmit={submitCity}>
+      <input 
+        className="weather-input" 
+        type="text" 
+        onChange={newLocation} 
+        placeholder="Your Town" 
+        value={location}
+        onFocus={(e) => e.target.select()}
+      >
+      </input>
+      <button type="submit"><CheckIcon /></button>
+    </form>
   )
 };
 
