dd49067389d373a20e3b8e6c933decb5a9b28d8e
David <davidirvin47@gmail.com>
2021-08-01 14:55:51 -0600

    Add nav and pull in context

--- client/src/components/EditProfile.js
+++ client/src/components/EditProfile.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Alert from "../components/Alert"
+import { Link } from 'react-router-dom'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import AuthContext from "../context/AuthContext"
 
 const EditProfile = () => {
+    const authContext = useContext(AuthContext)
+    const { name, email } = authContext.user
+
     return (
         <div className="position-div inside-cover">
             <div className="grid-div">
                 <div className="page">
                     <div className="heading">
+                        <Link className="profile-nav" to="/today">
+                            <ArrowBackIosIcon></ArrowBackIosIcon>
+                            Back
+                        </Link>
                         <h1 className="profile-heading">Profile Settings</h1>
                     </div>
                     <div className="pattern__wrapper" style={{ overflowY: "auto", overflowX: "visible" }}>
-                        <div className="pattern">
+                        <div className="pattern" style={{ repeatingLinearGradient: "unset" }}>
                             <img
                                 src="./images/Bald-Eagle.png"
                                 className="watermark"
                                 alt="watermark"
                             />
+                            <div className="content">
+                                <div>
+                                    {name}
+                                    {email}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
