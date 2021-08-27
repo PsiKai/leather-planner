08ab2a4b8f9a9837f1151d5dba567b4bc05a0550
David <davidirvin47@gmail.com>
2021-08-26 21:48:21 -0600

    Install react datepicker and basic styling

--- client/src/components/Datepicker.js
+++ client/src/components/Datepicker.js
@@ -1,50 +1,45 @@
-import React, {useContext, useRef} from 'react';
+import React, { useContext, useRef, useState } from 'react';
 // import TextField from '@material-ui/core/TextField';
 import AppContext from "../context/AppContext";
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css";
 
 const Datepicker = () => {
     const appContext = useContext(AppContext);
     const calendar = useRef();
+    const [date, setDate] = useState(new Date())
 
     const newDay = (e) => {
-        const regexDate = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/;
-        const validated = regexDate.test(e.target.value)
+        console.log(e.toLocaleDateString());
+        setDate(e)
+        // const regexDate = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/;
+        
+        // const validated = regexDate.test(e.toLocaleDateString())
         // console.log(validated);
-        if (validated) {
-            var date = new Date(e.target.value.replace(/-/g, "/"));
+        // if (validated) {
+        //     var date = new Date(e.target.value.replace(/-/g, "/"));
             var options = {day: '2-digit', month: 'short', year: 'numeric'};
-            var resultDate = date.toLocaleDateString('en-US', options).replace(/,/g, "").replace(/ /g, "-");
+            var resultDate = e.toLocaleDateString('en-US', options).replace(/,/g, "").replace(/ /g, "-");
+            console.log(resultDate);
             appContext.getList(resultDate)
-        }
+        // }
     }
-    const date = new Date().toLocaleDateString("en-US", 
-                    {day: "2-digit", 
-                    month: "2-digit", 
-                    year: "numeric"
-                    }).split("/");
-    const defaultDate = date[2] + "-" + date[0] + "-" + date[1];
+    // const date = new Date().toLocaleDateString("en-US", 
+    //                 {day: "2-digit", 
+    //                 month: "2-digit", 
+    //                 year: "numeric"
+    //                 }).split("/");
+    // const defaultDate = date[2] + "/" + date[0] + "/" + date[1];
 
     return (
         <div className="date-label">
-            <CalendarTodayIcon/>
-            {/* <TextField 
-                type="date"  
+            <DatePicker 
+                selected={date}
                 onChange={newDay}
-                defaultValue={defaultDate} 
-                // placeholder="Select"
-                InputLabelProps={{
-                    shrink: true
-                }}
-            /> */}
-            <input 
-                ref={calendar}
-                type="date" 
-                onChange={newDay} 
-                className="browser-default" 
-                defaultValue={defaultDate}
-                placeholder="yyyy-mm-dd"
-            />
+                className={"browser-default"}
+            /> 
+            <CalendarTodayIcon/>
         </div>
     )
 }
