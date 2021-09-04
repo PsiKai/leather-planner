5c9eb699c51d40fe645d2d1c667d451c98566425
David <davidirvin47@gmail.com>
2021-09-04 12:56:19 -0600

    Remove semicolons

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 if (process.env.NODE_ENV !== 'production') {
-  require("dotenv").config();
+  require("dotenv").config()
 }
 
 const express = require("express");
 const secure = require("ssl-express-www")
 const path = require("path")
-const connectDB = require("./db/db");
+const connectDB = require("./db/db")
 
-const app = express();
+const app = express()
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(secure)
 
-connectDB();
+connectDB()
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("Backend started on port " + port));
+app.listen(port, () => console.log("Backend started on port " + port))
 
 
 //sets routes for static build in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/build"))
 
-  app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")));
-  app.get("/today", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")));
+  app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")))
+  app.get("/today", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")))
 }
 
 
