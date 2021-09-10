2df67ea517563c63df7ab7e9b8d2905d56c0df91
David <davidirvin47@gmail.com>
2021-09-09 22:59:30 -0600

    Adjust routes to use inputText

--- routes/item/edit.js
+++ routes/item/edit.js
@@ -7,11 +7,11 @@ const auth = require("../../middleware/auth")
 const List = require("../../db/models/list")
 
 router.post("/", auth, (req, res) => {
-    const { list, item, id } = req.body
+    const { list, inputText, id } = req.body
   
     List.findOneAndUpdate(
         { "user": req.user.id, "name": list, "items._id": ObjectId(id) }, 
-        { "$set": { "items.$.item": item } },
+        { "$set": { "items.$.item": inputText } },
         { new: true },
         (err, newList) => {
             if (err) {
@@ -19,11 +19,7 @@ router.post("/", auth, (req, res) => {
                 res.status(500).json({msg: "Error updating list item"})
             } else {
                 console.log(("Item edited"));
-                // List.findOne({ "user": req.user.id, "name": list }, (err, foundList) => {
-                //     if (err) console.log(err);
-                //     res.status(200).send({ list, items: [...foundList.items] })
-                // })
-                res.status(200).send({ newList })
+                res.status(200).send(newList)
             }
         }
     )  
--- routes/item/new.js
+++ routes/item/new.js
@@ -7,19 +7,19 @@ const List = require("../../db/models/list")
 const { Item } = require("../../db/models/items")
 
 router.post("/", auth, (req, res) => {
-    const { item, list } = req.body
-    const newItem = new Item({ item, style: "", moved: false })
+    const { inputText, list } = req.body
+    const newItem = new Item({ item: inputText, style: "", moved: false })
 
     List.findOneAndUpdate(
       { "user": req.user.id, "name": list }, 
       { $push: { "items": newItem}},
       { new: true },
-      (err, foundList) => {
+      (err, newList) => {
         if (err) {
             console.log(err);
             res.status(500).json({ msg: "Error adding item to list" })
         }
-        res.status(201).send({ list, items: foundList.items })
+        res.status(201).send(newList)
     })
 })
 
