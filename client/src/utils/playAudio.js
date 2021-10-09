64f45404cbc53bec9ae91895b230b02a24c08c4c
David Irvin <63161310+PsiKai@users.noreply.github.com>
2021-10-09 17:32:59 -0600

    Fix audio play errors (#48)
    * Prevent audio play console error
    
    Extract audio from /today route into util function
    Use try/catch with async/await to prevent error message on fail
    
    * Extract profile page audio play to util function
    
    * Extract cross off audio into util function
    
    * Code cleanup, remove font awesome link

--- client/src/utils/playAudio.js
+++ client/src/utils/playAudio.js
@@ -1,22 +1,33 @@
 
 import pen1 from '../sounds/Pen1.wav';
 import pen2  from '../sounds/Pen2.wav';
+import pageFlip from "../sounds/page-flip.wav"
+import penCross from '../sounds/penCross1.wav';
 
 const penWriting = [pen1, pen2]
+let audio = new Audio()
+audio.volume = 0.1
 
-const playAudio = (action) => {
-    switch (action) {
-        case "write":
-            var audio = new Audio(
-                penWriting[Math.floor(Math.random() * penWriting.length)]
-            );
-            audio.volume = 0.1;
-            audio.play();
-            break;
-    
-        default:
-            break;
-    }
+const playAudio = async (action) => {
+    try {
+        switch (action) {
+            case "write":
+                audio.src = penWriting[Math.floor(Math.random() * penWriting.length)]
+                await audio.play();
+                break;
+            case "page":
+                audio.src = pageFlip
+                await audio.play()
+                break;
+            case "cross":
+                audio.src = penCross
+                audio.volume = 1
+                await audio.play()
+                break;
+            default:
+                break;
+        }
+    } catch (error) {}
 }
 
 export default playAudio
