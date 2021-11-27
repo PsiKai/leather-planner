fcee127b3eae1e21e586c63baebf4e6b7fef3e7d
David <davidirvin47@gmail.com>
2021-11-27 01:02:39 -0700

    Add row shading and average list size to table

--- client/src/components/admin/UserData.js
+++ client/src/components/admin/UserData.js
@@ -15,6 +15,11 @@ const UserData = () => {
     //     console.log(users);
     // }, [users])
 
+    const listAverage = (lists) => {
+        const total = lists.reduce((length, list) => length += list.items.length, 0)
+        return total ? (total / lists.length).toFixed(1) : null
+    }
+
     return (
         !loading ?
             <table className="user-dashboard">
@@ -24,6 +29,7 @@ const UserData = () => {
                         <th>Last Updated</th>
                         <th>Created Account</th>
                         <th>Total Lists</th>
+                        <th>Avg. List Size</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -33,6 +39,7 @@ const UserData = () => {
                         <td>{user.updatedAt ? new Date(user.updatedAt).toLocaleDateString() : "--"}</td>
                         <td>{user.createdAt && new Date(user.createdAt).toLocaleDateString()}</td>
                         <td>{user.lists.length}</td>
+                        <td>{listAverage(user.lists) || "--"}</td>
                     </tr>
                 ))}
                 </tbody>
