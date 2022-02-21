02cc2c10936c59bef9fb05dabd696810f58b80e7
david irvin <davidirvin47@gmail.com>
2022-02-21 12:01:04 -0700

    Update layout of table data and add safe traversal

--- client/src/components/admin/UserData.js
+++ client/src/components/admin/UserData.js
@@ -30,39 +30,41 @@ const UserData = () => {
     }
 
     const tableData = (key, value, index) =>  {
-        const updated = latestSnapshot.userData[index][key] === value ?
+        const updated = latestSnapshot.userData?.[index]?.[key] === value ?
             "" : "updated"
-        return <td className={updated}>{value}</td>
+        return <td className={`${updated} ${typeof(value)}`}>{value}</td>
     }
 
     const dateComparison = (newDate, i) => {
-        const updated = latestSnapshot.userData[i].lastLogin === newDate ?
+        const updated = latestSnapshot.userData[i]?.lastLogin === newDate ?
             "" : "updated"
-        return <td className={updated}>{new Date(newDate).toLocaleDateString()}</td>
+        return <td className={`${updated} date-of`}>{new Date(newDate).toLocaleDateString('en-US', dateOptions)}</td>
     }
+
+    const dateOptions = { month: '2-digit', day: '2-digit', year: 'numeric' }
 
     return (
         !loading ?
             <table className="user-dashboard">
                 <thead>
                     <tr>
-                        <th>User Name</th>
-                        <th>Logins</th>
-                        <th>Last Login</th>
-                        <th>Created Account</th>
-                        <th>Total Lists</th>
-                        <th>Avg. List Size</th>
+                        <th className='string'>User Name</th>
+                        <th className='date-of'>Created</th>
+                        <th className='date-of'>Last Login</th>
+                        <th className='number'>Logins</th>
+                        <th className='number'>Lists</th>
+                        <th className='number'>Avg List</th>
                     </tr>
                 </thead>
                 <tbody>
                 {users && users.map((user, i) => (
                     <tr key={user._id}>
                         {tableData("name", user.name, i)}
-                        {tableData("logins", user.logins, i)}
+                        <td className='date-of'>{user.createdAt && new Date(user.createdAt).toLocaleDateString('en-US', dateOptions)}</td>
                         {dateComparison(user.lastLogin, i)}
-                        <td>{user.createdAt && new Date(user.createdAt).toLocaleDateString()}</td>
+                        {tableData("logins", user.logins, i)}
                         {tableData("totalLists", user.lists.length, i)}
-                        <td>{listAverage(user.lists) || "--"}</td>
+                        <td className='number'>{listAverage(user.lists) || "---"}</td>
                     </tr>
                 ))}
                 </tbody>
