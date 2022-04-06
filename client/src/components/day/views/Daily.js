f05ec8c4ebdbbbfacef910d0720d9de6c4ce5b82
david irvin <davidirvin47@gmail.com>
2022-04-05 19:26:23 -0600

    Cleanup code

--- client/src/components/day/views/Daily.js
+++ client/src/components/day/views/Daily.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Content from '../content/Content'
 import { NavLink } from 'react-router-dom'
 import CalendarViewDayOutlinedIcon from '@material-ui/icons/CalendarViewDayOutlined';
-import ViewComfyIcon from '@material-ui/icons/ViewComfy';
 
 const Daily = () => {
   return (
     <div className="pattern">
         <div className='planner-toggle__wrapper'>
-            <NavLink activeClassName="planner-toggle" to="/planner/month">Month <ViewComfyIcon /></NavLink>
+            <NavLink activeClassName="planner-toggle" to="/planner/month">Month <CalendarViewDayOutlinedIcon /></NavLink>
         </div>
         <img 
             src="../../images/Bald-Eagle.webp" 
--- routes/list/month.js
+++ routes/list/month.js
@@ -7,13 +7,11 @@ const { sortListsByDate } = require("../../utils/sorting")
 const List = require("../../db/models/list")
 
 router.get("/:date", auth, async (req, res) => {
-    const date = new Date(req.params.date)
     const [month, day, year] = req.params.date.split("-")
     const monthList = new RegExp("^" + month + ".+" + year)
     try {
         let lists = await List.find({ user: req.user.id, name: { $regex: monthList } }).lean()
         lists = lists.sort(sortListsByDate)
-        console.log(lists);
         res.json({ lists })
     } catch (error) {
         res.json(error.message)
