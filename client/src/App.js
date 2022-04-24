3ab89ccc2b3a31453c03d1b0f28982d2654f75ec
david irvin <davidirvin47@gmail.com>
2022-04-24 13:43:55 -0600

    Cleanup analytics constant

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Planner from './components/day/Planner'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Planner from "./components/day/Planner"
 import Cover from "./components/cover/Cover"
-import EditProfile from "./components/profile/EditProfile";
-import UserAnalytics from "./components/admin/UserAnalytics";
-import AppState from './context/application/AppState'
-import AuthState from './context/authentication/AuthState'
+import EditProfile from "./components/profile/EditProfile"
+import UserAnalytics from "./components/admin/UserAnalytics"
+import AppState from "./context/application/AppState"
+import AuthState from "./context/authentication/AuthState"
 import setAuthToken from "./utils/setAuthToken"
 import PrivateRoute from "./components/routing/PrivateRoute"
-import './App.css';
-import AdminRoute from "./components/routing/AdminRoute";
+import "./App.css"
+import AdminRoute from "./components/routing/AdminRoute"
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token)
 }
 
 function App() {
-
   return (
     <AuthState>
       <AppState>
@@ -30,8 +29,7 @@ function App() {
         </Router>
       </AppState>
     </AuthState>
-
-  );
+  )
 }
 
-export default App;
+export default App
