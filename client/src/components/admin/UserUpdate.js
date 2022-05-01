791ae8586d3c29b9fb743802ddb0d0a08c2fb0b7
david irvin <davidirvin47@gmail.com>
2022-05-01 00:56:15 -0600

    Add styling to user update components

--- client/src/components/admin/UserUpdate.js
+++ client/src/components/admin/UserUpdate.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useContext, useState } from "react"
 import { submitUserInfoUpdates } from "../../utils/api/analytics"
 import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined"
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount"
+import PersonIcon from "@material-ui/icons/Person"
+import CheckBoxOutlineBlankOutlinedIcon from "@material-ui/icons/CheckBoxOutlineBlankOutlined"
+import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined"
 import AnalyticsContext from "../../context/analytics/AnalyticsContext"
 
 const UserUpdate = ({ setUserPopup, currentUser }) => {
@@ -10,7 +14,7 @@ const UserUpdate = ({ setUserPopup, currentUser }) => {
   useEffect(() => {
     if (currentUser) setNewUserData({ admin: currentUser.admin })
     else setUserPopup(false)
-  }, [currentUser, setUserPopup])
+  }, [currentUser, setUserPopup, currentUser.admin])
 
   const updateUserInfo = e => {
     const { name, value } = e.target
@@ -39,7 +43,9 @@ const UserUpdate = ({ setUserPopup, currentUser }) => {
     <div className="user-action__container">
       <div className="user-action">
         <div className="user-action--user">
-          <h2>{currentUser?.name}</h2>
+          <h2 className="user-action--info">
+            {currentUser.admin ? <SupervisorAccountIcon /> : <PersonIcon />} {currentUser?.name}
+          </h2>
           <p>{currentUser?.email}</p>
           <p className="user-activity">
             Last login: {`${Math.floor((new Date() - new Date(currentUser?.lastLogin)) / 1000 / 60 / 60 / 24)}`} days ago
@@ -52,22 +58,25 @@ const UserUpdate = ({ setUserPopup, currentUser }) => {
         <form className="user-update-form" onSubmit={submitUserInfo}>
           <h3>Update user info</h3>
           <label>
-            New Name
+            New Name:
             <input type="text" value={newUserData.name || ""} onChange={updateUserInfo} name="name" />
           </label>
           <label>
-            New Email
+            New Email:
             <input type="email" value={newUserData.email || ""} onChange={updateUserInfo} name="email" />
           </label>
           <label>
-            New Password
+            New Password:
             <input type="password" value={newUserData.password || ""} onChange={updateUserInfo} name="password" />
           </label>
-          <label>
-            {newUserData.admin ? "Revoke Admin" : "Grant Admin"}
-            <input type="checkbox" onChange={toggleAdmin} checked={newUserData.admin || false} name="admin" />
+          <label className="user-update--admin-label">
+            Admin Access:
+            <input id="admin-checkbox" type="checkbox" onChange={toggleAdmin} checked={newUserData.admin || false} name="admin" />
+            {newUserData.admin ? <CheckBoxOutlinedIcon /> : <CheckBoxOutlineBlankOutlinedIcon />}
           </label>
-          <button type="submit">Update User</button>
+          <button className="btn" type="submit">
+            Update User
+          </button>
         </form>
       </div>
     </div>
