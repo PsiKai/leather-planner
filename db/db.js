44451da7651f0f43f1bfe627ca6027b9593153da
David Irvin <davidirvin47@gmail.com>
2025-11-03 20:10:58 -0700

    Update mongoose connection options

--- db/db.js
+++ db/db.js
@@ -1,23 +1,20 @@
-if (process.env.NODE_ENV !== 'production') {
-  require("dotenv").config(); 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
 }
 
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 const mongoURI = process.env.MONGO_URI
 
 const connectDB = async () => {
-    try {
-      await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-      });
-      console.log("MongoDB connected");
-    } catch (err) {
-      console.error(err.message);
-      process.exit(1);
-    }
-  };
+  try {
+    await mongoose.connect(mongoURI, {
+      useUnifiedTopology: true,
+    })
+    console.log("MongoDB connected")
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+}
 
-module.exports = connectDB;
+module.exports = connectDB
